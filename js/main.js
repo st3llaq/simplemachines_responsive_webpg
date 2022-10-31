2df73f441d719c28a10cc0ea5b693fa3c4784363
st3llaq <stellaq@uw.edu>
2022-10-31 00:02:46 -0700

    add folders

---
+++ js/main.js
@@ -0,0 +1,50 @@
+let icon = document.getElementsByClassName("icon")[0];
+
+
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function responsive_control() {
+  let x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+
+}
+icon?.addEventListener('click', responsive_control);
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+ 
+  slides[1].style.display="block"; 
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  if (slides[slideIndex-1] && slides[slideIndex-1].style) {
+    slides[slideIndex-1].style.display="block";
+}
+
+  dots[slideIndex-1].className += " active";
+}
